5d9986cd7cc95b840098abb4c0b318d7049bd6dd
abdq293 <abd.kassar10@outlook.com>
2023-12-26 12:57:15 +0300

    finished hiring process without AI.

--- HR/src/main/java/com/senior/hr/model/PreviousProject.java
+++ HR/src/main/java/com/senior/hr/model/PreviousProject.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.proxy.HibernateProxy;
 import org.hibernate.type.SqlTypes;
 
 import java.util.Objects;
